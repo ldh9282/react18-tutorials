8bbc56cc54216fc9fd9ef8d5352005a6aecd8dbc
ldh9282 <ldh9282@naver.com>
2023-05-10 23:09:10 +0900

    2023-05-10 study cafe

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDom from 'react-dom/client'
 
-function Greeting() {
-    return <h2>My First Component</h2>;
+import './css/index.css'
+
+function BookList() {
+    return (
+        <section>
+            <Book 
+                img={{ src: "https://m.media-amazon.com/images/I/51ZLFV0ZBwL._SX375_BO1,204,203,200_.jpg", alt: "The Legend of Zelda™: Tears of the Kingdom" }} 
+                author={'Piggyback'} 
+                title={'The Legend of Zelda™: Tears of the Kingdom'} />
+            <Book 
+                title={'title2'}
+                author={'author2'} />
+            <Book 
+                author={'author3'} />
+        </section>        
+    )
 }
 
-export default Greeting;
+const Book = ({img, title, author}) => {
+    return (
+        <article>
+            {img ? <Img src={img.src} alt={img.alt} /> : <p>image placeholder</p>}
+            <p>
+                <Title title={title} />
+                <Author author={author} />
+            </p>
+        </article>
+    )
+}
+const Img = ({src, alt}) => {
+    return <img src={src} alt={alt} />
+}
+const Title = ({title}) => {
+    return <span className="h2">{title ? title : 'title placeholder'}</span>
+}
+
+const Author = ({author}) => {
+    return <span className="h3"> ({author ? author : 'author placeholder'})</span>
+}
+
+export default BookList;
 
 
 const root = ReactDom.createRoot(document.querySelector('#root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
